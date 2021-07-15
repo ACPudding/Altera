fb3372051be2195a004b1831c2265da124bd0153
Gilgamesh <2319545161@qq.com>
2021-07-15 22:35:28 +0800

    Revert "回滚组件更新操作."
    This reverts commit f8483f5ccb3e0a9a9bb2b87510ba6a6510b1fbb9.

--- Properties/CommonStrings.cs
+++ Properties/CommonStrings.cs
@@ -2,7 +2,7 @@
 {
     class CommonStrings
     {
-        public static string Version = "版本号:  v3.5.2";
-        public static string VersionTag = "v3.5.2";
+        public static string Version = "版本号:  v3.5.0";
+        public static string VersionTag = "v3.5.0";
     }
 }
