9d9772ed8bbaf17cadbddde2456aad79407975a0
Charlemagne <miaomiao2130@gmail.com>
2025-06-04 20:13:00 +0800

    修正新从者六维面板部分内容无法显示的问题. 优化代码.

--- Properties/CommonStrings.cs
+++ Properties/CommonStrings.cs
@@ -2,7 +2,7 @@
 {
     class CommonStrings
     {
-        public static string Version = "版本号:  v3.25.5";
-        public static string VersionTag = "v3.25.5";
+        public static string Version = "版本号:  v3.25.6";
+        public static string VersionTag = "v3.25.6";
     }
 }
